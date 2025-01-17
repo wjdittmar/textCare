75a9587379983b45055e680d9aee1f2ed1974b6e
James Dittmar <wjdittmar@gmail.com>
2025-01-17 13:59:54 -0800

    allow auth header and import react query

--- back/cmd/api/middleware.go
+++ back/cmd/api/middleware.go
@@ -155,6 +155,7 @@ func (app *application) requirePermission(code string, next http.HandlerFunc) ht
 func (app *application) enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		next.ServeHTTP(w, r)
 	})
 }
--- front/textcare/next.config.ts
+++ front/textcare/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: "export",
+    reactStrictMode: true,
     /* config options here */
 };
 
