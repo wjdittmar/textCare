487e1af60f7c96a1b865e4d58c70a79ae2dd5acf
James Dittmar <wjdittmar@gmail.com>
2025-01-17 15:03:10 -0800

    temp disable strict mode to enable build

--- front/textcare/next.config.ts
+++ front/textcare/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: "export",
-    reactStrictMode: true,
+    //reactStrictMode: true,
     /* config options here */
 };
 
