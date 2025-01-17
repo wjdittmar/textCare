630cc14ed686acab7bbd5b1f90d4d2d39eb98438
James Dittmar <wjdittmar@gmail.com>
2025-01-17 13:05:29 -0800

    add initial nextjs project

--- back/cmd/api/routes.go
+++ back/cmd/api/routes.go
@@ -3,6 +3,8 @@ package main
 import (
 	"github.com/julienschmidt/httprouter"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 func (app *application) routes() http.Handler {
@@ -17,5 +19,19 @@ func (app *application) routes() http.Handler {
 
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
+
+	// serve the nextjs frontend
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		outDir := "../front/textcare/out"
+		requestedPath := filepath.Join(outDir, r.URL.Path)
+
+		info, err := os.Stat(requestedPath)
+		if err == nil && !info.IsDir() {
+			http.ServeFile(w, r, requestedPath)
+			return
+		}
+		// Otherwise serve index.html
+		http.ServeFile(w, r, filepath.Join(outDir, "index.html"))
+	})
 	return app.recoverPanic(app.enableCORS(app.rateLimit(app.authenticate(router))))
 }
