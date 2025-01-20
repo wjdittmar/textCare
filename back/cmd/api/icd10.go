544b09567306cbea695b9a80ed34aa7feada5b71
James Dittmar <wjdittmar@gmail.com>
2025-01-20 11:46:52 -0800

    implement query limit

--- back/cmd/api/icd10.go
+++ back/cmd/api/icd10.go
@@ -1,19 +1,33 @@
 package main
 
 import (
+	"github.com/wjdittmar/textCare/back/internal/validator"
 	"net/http"
 )
+
+const maxQueryResults = 10
 
 func (app *application) getIcd10Handler(w http.ResponseWriter, r *http.Request) {
 	var input struct {
 		QueryString string
+		Limit       int
 	}
-
+	v := validator.New()
 	qs := r.URL.Query()
 
 	input.QueryString = app.readString(qs, "qs", "")
+	input.Limit = app.readInt(qs, "limit", 10, v)
 
-	codes := app.models.ICD10.SearchByDescription(input.QueryString, 10)
+	if !v.Valid() || input.Limit < 1 {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	if input.Limit > maxQueryResults {
+		input.Limit = 10
+	}
+
+	codes := app.models.ICD10.SearchByDescription(input.QueryString, input.Limit)
 
 	err := app.writeJSON(w, http.StatusOK, envelope{"icd10codes": codes}, nil)
 	if err != nil {
