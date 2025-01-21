021b6c1501167bbd143d211bf47616a2fa083bc2
James Dittmar <wjdittmar@gmail.com>
2025-01-20 18:07:41 -0800

    comments

--- back/cmd/api/main.go
+++ back/cmd/api/main.go
@@ -45,7 +45,6 @@ func main() {
 
 	cfg.db.dsn = os.Getenv("DB_DSN")
 
-
 	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
 	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
 	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
