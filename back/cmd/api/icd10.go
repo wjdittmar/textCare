d0d5b52b21200a8e652cd0a041b61cb6b5c7efa0
James Dittmar <wjdittmar@gmail.com>
2025-02-05 17:02:59 -0800

    revert icd10 changes

--- back/cmd/api/icd10.go
+++ back/cmd/api/icd10.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"github.com/wjdittmar/textCare/cmd/api/client"
+	"github.com/wjdittmar/textCare/back/internal/validator"
 	"net/http"
 )
+
+const maxQueryResults = 10
 
 func (app *application) getIcd10Handler(w http.ResponseWriter, r *http.Request) {
 	var input struct {
@@ -25,16 +27,11 @@ func (app *application) getIcd10Handler(w http.ResponseWriter, r *http.Request) 
 		input.Limit = 10
 	}
 
-	terminologyClient := client.NewTerminologyClient(app.config.TerminologyServiceURL)
+	codes := app.models.ICD10.SearchByDescription(input.QueryString, input.Limit)
 
-	codes, err := terminologyClient.Search(r.Context(), "ICD10", input.QueryString, input.Limit)
-	if err != nil {
-		app.serverErrorResponse(w, r, err)
-		return
-	}
-
-	err = app.writeJSON(w, http.StatusOK, envelope{"icd10codes": codes}, nil)
+	err := app.writeJSON(w, http.StatusOK, envelope{"icd10codes": codes}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
+
 }
